ed2e1534dca2694a5b8e59127b8fb17e94b0a22e
Karen-96 <apablazakaren8@gmail.com>
2020-10-05 20:39:29 -0300

    Usuario modelo

---
+++ src/server/stateless/UsuarioServiceBean.java
@@ -0,0 +1,33 @@
+package stateless;
+
+import javax.persistence.EntityManager;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.PersistenceContext;
+import javax.persistence.NoResultException;
+
+import model.Usuario;
+import stateless.UsuarioService;
+
+
+
+@Stateless
+public class UsuarioServiceBean implements UsuarioService {
+    @PersistenceContext(unitName = "mrplite")
+    protected EntityManager em;
+
+    public EntityManager getEntityManager(){
+        return em;
+    }
+
+    @Override
+    public Usuario create(Usuario usuario) {
+        getEntityManager().persist(usuario);
+        return usuario;
+    }
+
+
+
+    
+}
