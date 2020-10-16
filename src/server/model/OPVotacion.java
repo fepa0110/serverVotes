8faf18f1559a85ae7d456fa9fcde8cd0202027fe
Nico-98 <nicolasbaldoni07@gmail.com>
2020-10-16 18:56:29 -0300

    import OPVotacion

--- src/server/model/OPVotacion.java
+++ src/server/model/OPVotacion.java
@@ -8,6 +8,7 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Column;
 import javax.persistence.JoinColumn;
 import javax.persistence.CascadeType;
 import java.util.Set;
